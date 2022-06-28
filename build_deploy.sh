0de7da1e555050eb0889f44092033b8295accfce
Viliam Krizan <vkrizan@redhat.com>
2022-06-28 16:23:12 +0200

    build: create build_deploy.sh for CI
    to be used with internal build system
    
    RHICOMPL-3038

---
+++ build_deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+login_container_registry() {
+
+    local USER="$1"
+    local PASSWORD="$2"
+    local REGISTRY="$3"
+
+    podman login "-u=${USER}" "--password-stdin" "$REGISTRY" <<< "$PASSWORD"
+}
+
+IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+
+login_container_registry "$QUAY_USER" "$QUAY_TOKEN" "$QUAY_REGISTRY"
+login_container_registry "$RH_REGISTRY_USER" "$RH_REGISTRY_TOKEN" "$REDHAT_REGISTRY"
+
+VERSION=$IMAGE_TAG make podman-build podman-tag-latest podman-push podman-push-latest
