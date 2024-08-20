b0858a26640d37c3826bfed398f9f39bdff7ef96
Janos Laszlo Vasik <jvasik@redhat.com>
2024-08-20 16:52:39 +0200

    fix: check only last jobs created

--- stage_test.sh
+++ stage_test.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# New Floorist image used by the operator
 NEW_FLOORIST_IMG="$(
-    oc get deployment.apps -o jsonpath='{.items[?(.metadata.name=="floorist-operator-controller-manager")].spec.template.spec.containers[?(.name=="manager")].env[?(.name=="FLOORIST_IMAGE")].value}{"\n"}'
+    oc get deployment.apps -l "control-plane=controller-manager" \
+    -o jsonpath='{.items[].spec.template.spec.containers[?(.name=="manager")].env[?(.name=="FLOORIST_IMAGE")].value}{"\n"}'
 ):$(
-    oc get deployment.apps -o jsonpath='{.items[?(.metadata.name=="floorist-operator-controller-manager")].spec.template.spec.containers[?(.name=="manager")].env[?(.name=="FLOORIST_IMAGE_TAG")].value}{"\n"}'
+    oc get deployment.apps -l "control-plane=controller-manager" \
+    -o jsonpath='{.items[].spec.template.spec.containers[?(.name=="manager")].env[?(.name=="FLOORIST_IMAGE_TAG")].value}{"\n"}'
 )"
 
-NAMESPACED_CRONJOBS=$(oc get cronjobs -l "service=floorist" \
-                     -o jsonpath='{range .items[*]}{.metadata.namespace}:{.metadata.name}{"\n"}{end}' --all-namespaces)
+MANAGER_TIMESTAMP=$(
+    oc get deployment.apps -l "control-plane=controller-manager" \
+    -o jsonpath='{.items[].metadata.managedFields[?(.manager=="kubectl-client-side-apply")].time}{"\n"}'
+)
+
+NAMESPACED_CRONJOBS=$(
+    oc get cronjobs -l "service=floorist" -o \
+    jsonpath='{range .items[*]}{.metadata.namespace};{.metadata.name}{"\n"}{end}' --all-namespaces
+)
 
 if [[ -z "$NAMESPACED_CRONJOBS" ]]; then
     echo "ERROR: no cronjobs found"
@@ -24,27 +32,33 @@ IFS=$'\n'
 
 NAMESPACED_CRONJOBS=($NAMESPACED_CRONJOBS)
 for NAMESPACED_CRONJOB in "${NAMESPACED_CRONJOBS[@]}"; do
-    NAMESPACE=$(echo "$NAMESPACED_CRONJOB" | awk -F ':' '{print $1}')
-    CRONJOB=$(echo "$NAMESPACED_CRONJOB" | awk -F ':' '{print $2}')
+    NAMESPACE=$(echo "$NAMESPACED_CRONJOB" | cut -d ";" -f 1)
+    CRONJOB=$(echo "$NAMESPACED_CRONJOB" | cut -d ";" -f 2)
 
-    SUCCESS=$(oc get job -l "pod=$CRONJOB" -o jsonpath='{range .items[*]}{..succeeded}{"\n"}{end}' -n "$NAMESPACE")
+    JOBS=$(oc get jobs -l "pod=$CRONJOB" -o jsonpath='{range .items[*]}{.status.succeeded};{.metadata.name}{"\n"}{end}' -n "$NAMESPACE")
 
-    if [[ -z "$SUCCESS" ]]; then
-        echo "ERROR: cronjob $CRONJOB has not created any jobs in namespace $NAMESPACE"
-        exit 1
-    fi
+    # Testing if any jobs that have failed were created after the operator's last deployment
+    JOBS=($JOBS)
+    for JOB_RESULT in "${JOBS[@]}"; do
+        RESULT=$(echo "$JOB_RESULT" | cut -d ";" -f 1)
 
-    SUCCESS=($SUCCESS)
-    for SUCCESS_BOOLEAN in "${SUCCESS[@]}"; do
-        if [[ "$SUCCESS_BOOLEAN" != "1" ]]; then
-            echo "ERROR: cronjob $CRONJOB has a failed run in namespace $NAMESPACE"
-            exit 1
+        if [[ "$RESULT" != "1" ]]; then
+            JOB=$(echo "$JOB_RESULT" | cut -d ";" -f 2)
+            JOB_FAILURE_TIME=$(oc get job "$JOB" -o jsonpath='{.status.startTime}{"\n"}' -n "$NAMESPACE")
+
+            if [[ "$JOB_FAILURE_TIME" > "$MANAGER_TIMESTAMP" ]]; then
+                echo "ERROR: cronjob $CRONJOB has a failed run in namespace $NAMESPACE"
+                echo "Failed job: $JOB"
+                exit 1
+            fi
         fi
     done
 
     # Comparing operator's image with the image used by the newest job
-    LATEST_JOB_IMAGE=$(oc get job -l "pod=${CRONJOB}" --sort-by=.metadata.creationTimestamp -o \
-                        jsonpath='{.items[-1].spec.template.spec.containers[].image}{"\n"}' -n $NAMESPACE)
+    LATEST_JOB_IMAGE=$(
+        oc get job -l "pod=${CRONJOB}" --sort-by=.metadata.creationTimestamp \
+        -o jsonpath='{.items[-1].spec.template.spec.containers[].image}{"\n"}' -n $NAMESPACE
+    )
 
     if [[ "$NEW_FLOORIST_IMG" != "$LATEST_JOB_IMAGE" ]]; then
         echo "ERROR: newest job of cronjob $CRONJOB in namespace $NAMESPACE is not configured with the newest Floorist image"
